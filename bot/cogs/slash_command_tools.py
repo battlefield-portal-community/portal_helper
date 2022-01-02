933ba4af78efa0ccf2ac33e5cb7db94306a683ba
p0lygun <solankivibhakar82@gmail.com>
2022-01-03 00:19:57 +0530

    made tools command better; added gametools

--- bot/cogs/slash_command_tools.py
+++ bot/cogs/slash_command_tools.py
@@ -14,53 +14,51 @@ class ToolsCog(commands.Cog):
 
     @slash_command(name="tools", guild_ids=[devGuildID] if os.getenv("PD_DEVELOPMENT") == "True" else None)
     async def tools(self, ctx, hidden: Option(bool, "If set to true only you can see it", required=False)):
+        fields = [{
+            "name": "BF2042-Portal-Extensions",
+            "value": "Extension to add various QOL features to portal's logic editor",
+            "url": "https://github.com/LennardF1989/BF2042-Portal-Extensions",
+            "user_id": 182574451366428672
+        }, {
+            "name": "Portal-unleashed",
+            "value": "Chrome extension allowing you to make your Portal Experience directly in "
+                     "pseudo-Javascript from a VSCode editor in your browser",
+            "url": "https://github.com/Ludonope/BFPortalUnleashed",
+            "user_id": 145955873913700352
+        }, {
+            "name": "Game Tools",
+            "value": "Community Network aims to provide public tools for different games. Starting with Battlefield, "
+                     "we are bringing back online concurrent player stats, as well as personal stats and much more in"
+                     " future",
+            "url": "https://gametools.network/",
+            "user_id": 140391046822494208
+
+        }, {
+            "name": "Battlefield Portal Blocks",
+            "value": "A repository of some useful Portal Rule Editor Blocks",
+            "url": "https://github.com/Andygmb/Battlefield-Portal-Blocks",
+            "user_id": 152173878376923136
+        }, {
+            "name": "Portal Helper",
+            "value": "This Bot which gives you access to various commands to help with portal",
+            "url": "https://github.com/p0lygun/portal-docs-bot",
+            "user_id": 338947895665360898
+        }]
+        thumbnail_url = "https://cdn.discordapp.com/attachments/" \
+                        "908104736455155762/912999248910495784/Animation_logo_discord.gif"
         embed = Embed(
             title="By the Community For the Community",
             url="https://bfportal.gg/",
-            description="**A list of tools/resources made by community**\u200B",
+            description="**A list of tools/resources made by community**\n\n" +
+                        "\n\n".join(
+                            [
+                                f'**[{i["name"]}]({i["url"]})**\n{i["value"]}\nMaintained by <@{i["user_id"]}>'
+                                for i in fields
+                            ]
+                        ),
             color=int(random.choice(self.bot.colors), 16),
         )
-        spacer = {
-            "name": "\u200B",
-            "value": "\u200B"
-        }
-        fields = [
-            {
-                "name": "BF2042-Portal-Extensions\n",
-                "value": "Extension to add various QOL features to portal's logic editor\n"
-                         "[github.com/LennardF1989/BF2042-Portal-Extensions]("
-                         "https://github.com/LennardF1989/BF2042-Portal-Extensions)\nMade by [ "
-                         "LennardF1989#3733 ]",
-                "inline": False
-            },
-            {
-                "name": "Portal-unleashed",
-                "value": "\nChrome extension allowing you to make your Portal Experience directly in "
-                         "pseudo-Javascript from a VSCode editor in your browser\n Download "
-                         "from:- [github.com/Ludonope/BFPortalUnleashed](https://github.com/Ludonope/BFPortalUnleashed)\n"
-                         "Made by [ ludonope#4197 ]\u200B",
-                "inline": False
-            },
-            {
-                "name": "Battlefield Portal Blocks",
-                "value": "A repository of some useful Portal Rule Editor Blocks\n["
-                         "github.com/Andygmb/Battlefield-Portal-Blocks]( "
-                         "https://github.com/Andygmb/Battlefield-Portal-Blocks) \nmaintained by [ andy#0743 ]",
-                "inline": False
-            }
-        ]
-        footer_text = "And lastly this bot which was made by [ gala#8316 ] " \
-                      "\nMaintained at\n https://github.com/p0lygun/portal-docs-bot"
-        thumbnail_url = "https://cdn.discordapp.com/attachments/" \
-                        "908104736455155762/912999248910495784/Animation_logo_discord.gif"
-
-        for i in fields:
-            embed.add_field(**i)
-            embed.add_field(**spacer)
-
         embed.set_thumbnail(url=thumbnail_url)
-        embed.set_footer(text=footer_text)
-
         await ctx.respond(
             embeds=[embed], ephemeral=hidden
         )
@@ -68,4 +66,3 @@ class ToolsCog(commands.Cog):
 
 def setup(bot):
     bot.add_cog(ToolsCog(bot))
-
