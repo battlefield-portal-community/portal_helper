b1bb3dda76af3bb63981c85f278f33efcc00e9f5
p0lygun <solankivibhakar82@gmail.com>
2022-06-17 00:33:06 +0530

    move reload github to SlashCommandGroup

--- bot/cogs/slash_command_reload_cogs.py
+++ bot/cogs/slash_command_reload_cogs.py
@@ -1,7 +1,7 @@
 from loguru import logger
-from discord.commands import slash_command, permissions, Option
 from discord.ext import commands
 from discord.embeds import Embed
+from discord.commands import SlashCommandGroup
 
 from utils.helper import devGuildID
 
@@ -10,76 +10,10 @@ class ReloadCogs(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @slash_command(name="reload_cogs", guild_ids=[devGuildID], default_permission=False)
-    @permissions.is_owner()
-    async def reload_cogs(
-            self,
-            ctx,
-            module: Option(str, "Name of the module to reload")
-    ):
-        f"""
-        Reload a gives extension to enable Hot reload
-    
-        :param ctx: context 
-        :param module: name of the module must be one of {self.bot.cogs_list}
-        :return: None
-        """
-        if module not in self.bot.cogs_list:
-            embed = Embed(
-                    title=":red_circle: :red_circle: Command Failed :red_circle: :red_circle: ",
-                    description=f"Invalid cog {module} valid cogs are :-",
-                    color=int("ff0000", 16)
-                )
+    reload_group = SlashCommandGroup("reload", "reload parts of the bot", checks=[commands.is_owner().predicate])
 
-            await ctx.respond(embeds=[embed])
-            return
-        elif module == "bot.cogs.slash_command_reload_cogs":
-            await ctx.respond(embeds=[
-                Embed(
-                    title="Wow really!!! ?",
-                    description=f"U can't unload the un-loader itself LOL",
-                    color=int("ff0000", 16)
-                )],
-            )
-            return
-
-        logger.debug(f"Trying to reload cog {module}")
-        try:
-            self.bot.unload_extension(module)
-        except BaseException as e:
-            logger.critical(f"Error {e} happened when unloading cog {module}")
-            await ctx.respond(embeds=[
-                Embed(
-                    title=f"Error Happened when unloading cog {module}",
-                    description=f"{e}",
-                    color=int("ff0000", 16)
-                )]
-            )
-
-        try:
-            self.bot.load_extension(module)
-        except BaseException as e:
-            logger.critical(f"Error {e} happened when loading cog {module}")
-            await ctx.respond(embeds=[
-                Embed(
-                    title=f"Error Happened when loading cog {module}",
-                    description=f"{e}",
-                    color=int("ff0000", 16)
-                )]
-            )
-
-        logger.debug(f"Successfully reloaded cog {module}")
-
-        await ctx.respond(embeds=[
-            Embed(
-                title=f"Successfully reloaded cog {module}",
-                description=f":white_check_mark:",
-            )]
-        )
-
-    @slash_command(name="reload_github", guild_ids=[devGuildID], default_permission=False)
-    @permissions.is_owner()
-    async def reload(self, ctx):
+    @reload_group.command(description="Reloads GitHub's Data")
+    async def github(self, ctx):
         logger.debug(f"trying to update github data command called by {ctx.author.id}")
         if ctx.author.id == 338947895665360898:
             try:
@@ -110,6 +44,7 @@ class ReloadCogs(commands.Cog):
                     colour=int("ff0000", 16)
                 )]
             )
+
 
 def setup(bot):
     bot.add_cog(ReloadCogs(bot))
